6f4c542a2958c9f1b490d9c8fdb1c1e8de5ba3a9
Madhav <madhav.kabra@dechea.com>
2022-11-12 17:25:56 +0530

    Add skills

--- src/resumeData.jsx
+++ src/resumeData.jsx
@@ -6,6 +6,7 @@ const resumeData = {
   "name": "Madhav Kabra",
   "role": "Senior Frontend Developer",
   "linkedinId": "https://www.linkedin.com/in/madhavkabra/",
+  "whatsappLink": " https://wa.me/9229996730?text=Hey%20Madhav,%20Lets%20connect",
   "skypeid": "live:madhav.kabra1100",
   "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
   "socialLinks": [
@@ -57,63 +58,171 @@ const resumeData = {
       "specialization": "Some specialization",
       "MonthOfLeaving": "Jan",
       "YearOfLeaving": "2018",
-      "Achievements": "Some Achievements"
+      "responsibilites": ""
     },
     {
       "CompanyName": "Blockapps Brooklyn, New York, United States",
       "specialization": "Backend developer",
       "MonthOfLeaving": "Aug",
       "YearOfLeaving": "2019",
-      "Achievements": `
-      - A+ Grade — Performance evaluation from past employers.
-      - Implemented role-based access control using Nodejs.
-      - Developed a finite state automaton using smart contracts.
-      - Added data validation and API level test cases to test both 
-failure cases and happy flow.
-      - Store the data on IPFS, in a distributed network.
-`
+      "responsibilites": [`A+ Grade — Performance evaluation from past employers.`, `Implemented role-based access control using Nodejs.`,
+        `Deeloped a finite state automaton using smart contracts.`, `Added data validation  and API level test cases to test both 
+failure c a ses and happy flow.`, `Store the data on IPFS, in a distributed network.`,
+      ]
     }
   ],
-  "skillsDescription": "Your skills here",
   "skills": [
     {
-      "skillname": "HTML5"
+      title: "Frontend",
+      list: [{
+        "skillname": "HTML5",
+        "skillPercent": 0.9
+      },
+      {
+        "skillname": "CSS",
+        "skillPercent": 0.8
+      },
+      {
+        "skillname": "React.js",
+        "skillPercent": 0.9
+      },
+      {
+        "skillname": "Bit.dev",
+        "skillPercent": 0.8
+      },
+      {
+        "skillname": "Rest API, GraphQL",
+        "skillPercent": 0.7
+      },
+      {
+        "skillname": "Storybook",
+        "skillPercent": 0.5
+      },
+      {
+        "skillname": "Internationalization, Localization",
+        "skillPercent": 0.8
+      },
+      {
+        "skillname": "Sonar cloud",
+        "skillPercent": 0.8
+      },
+      {
+        "skillname": "Micro-frontends",
+        "skillPercent": 0.9
+      },
+      {
+        "skillname": "Google Analytics",
+        "skillPercent": 0.9
+      },
+      {
+        "skillname": "Monolithic frontends",
+        "skillPercent": 0.9
+      }],
     },
     {
-      "skillname": "CSS"
+      title: "Testing",
+      list: [
+        {
+          "skillname": "Browser automation testing using Cypress",
+          "skillPercent": 0.7
+        },
+        {
+          "skillname": "Testcafe and chromium drivers",
+          "skillPercent": 0.8,
+        },
+        {
+          "skillname": "Unit tests — Jest, React testing-library, Chai, Mocha",
+          "skillPercent": 0.9,
+        },
+        {
+          "skillname": "Snapshot testing",
+          "skillPercent": 0.8
+        }]
     },
     {
-      "skillname": "Reactjs"
+      title: "Backend",
+      list: [
+        {
+          "skillname": "Node.js",
+          "skillPercent": 0.7
+        },
+        {
+          "skillname": "Joi",
+          "skillPercent": 0.5,
+        }]
+    },
+    {
+      title: "Database",
+      list: [
+        {
+          "skillname": "FaunaDB",
+          "skillPercent": 0.8
+        },
+        {
+          "skillname": "MongoDB",
+          "skillPercent": 0.8
+        },
+        {
+          "skillname": "Firebase, Firestore",
+          "skillPercent": 0.7
+        }]
+    },
+    {
+      title: "Tools",
+      list: [
+        {
+          "skillname": "Git - GitHub, Bitbucket",
+          "skillPercent": 0.9
+        },
+        {
+          "skillname": "Trello",
+          "skillPercent": 0.8
+        },
+        {
+          "skillname": "Atlassian tools like Jira, Kanban board and Confluence",
+          "skillPercent": 0.9
+        }], 
     }
   ],
   "portfolio": [
-    {
+  {
+    
       "name": "project1",
-      "description": "mobileapp",
-      "imgurl": "images/portfolio/phone.jpg"
-    },
+        "description": "mobileapp",
+    "imgurl": "images/portfolio/phone.jpg"
+ },
     {
-      "name": "project2",
-      "description": "mobileapp",
+ 
+  "name": "project2",
+    "description": "mobileapp",
       "imgurl": "images/portfolio/project.jpg"
-    },
-    {
+  },
+  {
       "name": "project3",
-      "description": "mobileapp",
-      "imgurl": "images/portfolio/project2.png"
+ 
+  "description": "mobileapp",
+    "imgurl": "images/portfolio/project2.png"
     },
-    {
+   {
       "name": "project4",
-      "description": "mobileapp",
-      "imgurl": "images/portfolio/phone.jpg"
+        "description": "mobileapp",
+   
+    "imgurl": "images/portfolio/phone.jpg"
     }
-  ],
-  "testimonials": [
+  ],     
+"testimonials": [
     {
       "description": "A very dedicated engineer, with a very observant eye for code quality and good practice. He has a knack for quickly adapting to the development ecosystem and contributing, not only to the functionalities but taking initiatives to enforce best practices and follow them which reflects on his work.",
-      "name": "Karun Thomas, Manager, Talent500"
+       "name": "Karun Thomas, Manager, Talent500"
     },
   ]
-}
+    
+    
+    
+    
+    
+    
+    
+  }
 
-export default resumeData
+  export default resumeData
