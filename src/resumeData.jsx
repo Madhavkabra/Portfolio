b7408972cf8fcb003b966d3cd11ebf03267bc1c0
Madhav <madhav.kabra@dechea.com>
2022-11-12 15:28:47 +0530

    Initial ommit

---
+++ src/resumeData.jsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import UpworkIcon from './assets/upwork.svg'
+
+const resumeData = {
+  "imagebaseurl": "",
+  "name": "Madhav Kabra",
+  "role": "Senior Frontend Developer",
+  "linkedinId": "https://www.linkedin.com/in/madhavkabra/",
+  "skypeid": "live:madhav.kabra1100",
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/madhavkabra/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/Madhavkabra",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "skype",
+      "url": "https://join.skype.com/invite/uYOoOO90jxo3",
+      "className": "fa fa-skype"
+    },
+    {
+      "name": "upwork",
+      "url": "https://www.upwork.com/freelancers/~01e690d5a7b5d8b86c",
+      "image": UpworkIcon
+    }
+  ],
+  "aboutme": `Experienced Front End Developer, specializing in ⚛️React-based⚛️ Web Applications and can methodically break down and create COMPLEX APPLICATIONS as per YOUR REQUIREMENTS, using a wide variety of techniques and tools 🛠️.
+  I create responsive and performant REACT APPLICATIONS, which are responsive in all screen sizes 📱🖥️. I can re-engineer immensely complex designs and code, providing significantly improved performance.
+  I pride myself on designing intuitive interfaces that provides great USER EXPERIENCE.`,
+  "address": "India",
+  "website": "",
+  "education": [
+    {
+      "UniversityName": "Rajiv Gandhi Proudyogiki Vishwavidyalaya",
+      "specialization": "B. Tech. - Information Technology",
+      "MonthOfPassing": "August",
+      "YearOfPassing": "2019",
+      "Achievements": "NSS India — C Certified "
+    },
+    {
+      "UniversityName": "St. Norbert Senior Secondary School",
+      "specialization": "Mathematics",
+      "MonthOfPassing": "January",
+      "YearOfPassing": "2015",
+      "Achievements": "Some Achievements"
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "Some Company",
+      "specialization": "Some specialization",
+      "MonthOfLeaving": "Jan",
+      "YearOfLeaving": "2018",
+      "Achievements": "Some Achievements"
+    },
+    {
+      "CompanyName": "Some Company",
+      "specialization": "Some specialization",
+      "MonthOfLeaving": "Jan",
+      "YearOfLeaving": "2018",
+      "Achievements": "Some Achievements"
+    }
+  ],
+  "skillsDescription": "Your skills here",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Reactjs"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "project1",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    },
+    {
+      "name": "project2",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project.jpg"
+    },
+    {
+      "name": "project3",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project2.png"
+    },
+    {
+      "name": "project4",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "A very dedicated engineer, with a very observant eye for code quality and good practice. He has a knack for quickly adapting to the development ecosystem and contributing, not only to the functionalities but taking initiatives to enforce best practices and follow them which reflects on his work.",
+      "name": "Karun Thomas, Manager, Talent500"
+    },
+  ]
+}
+
+export default resumeData
